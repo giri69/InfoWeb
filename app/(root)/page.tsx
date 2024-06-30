12efee0a4b72c37967b3c15e014fdc0e4a44a97f
giridhar <giridharpp07@gmail.com>
2024-07-01 01:07:52 +0530

    added components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,8 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home()
 {
   return(
-  <UserButton afterSignOutUrl="/"/>
+  <h1 className="head-text text-left ">
+    Home
+  </h1>
  )
 }
